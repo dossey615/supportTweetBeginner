5578c9b549f3d424abcb599d35c5cce3ed4262e4
dossey615 <dossey615@gmail.com>
2021-12-18 15:01:48 +0900

    Initial Commit

---
+++ supportTweetBeginner/supportTweetBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  supportTweetBeginnerApp.swift
+//  supportTweetBeginner
+//
+//  Created by 土居将史 on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct supportTweetBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
